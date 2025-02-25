a7cd1e976ac2587749e8af7932b67c00c657bbdf
Arin0o <arinopeople@gmail.com>
2025-02-25 18:40:34 +0100

    requests versucht hinzuzufügen

--- ampapi/ampapi.py
+++ ampapi/ampapi.py
@@ -1,5 +1,11 @@
 import discord
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "lib")))
+
 import requests
+
 from redbot.core import commands, Config, tasks
 from redbot.core.bot import Red
 
